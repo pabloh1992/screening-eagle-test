2bfad817f40194d1ab1ccc1def4b12c406f63450
Pablo Henao Posada <pabloh1992@gmail.com>
2024-01-04 21:11:23 -0500

    Initial home screen implementation

---
+++ WeatherApp/app/src/main/java/com/pablodev/weatherapp/ui/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.pablodev.weatherapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pablodev.weatherapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private val binding by lazy {
+        FragmentHomeBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = binding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
